609f83d34c345dd6d276bec23ce9186e54cbc64c
Ibon2 <ibonlkz@gmail.com>
2022-08-12 12:42:50 +0200

    finishing Part 4 Form validation

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -1,29 +1,14 @@
 const express = require("express");
+const validateForm = require("../controlers/validateForm");
 const router = express.Router();
-const Yup = require("yup");
 
-const formSchema = Yup.object({
-    username: Yup.string()
-                .required('Username required')
-                .min(6, 'Username too short')
-                .max(28, 'Username too large'),
-    password: Yup.string()
-        .required('Password required')
-        .min(6, 'Password too short')
-        .max(28, 'Password too large'),
-});
+
+
 router.post("/login", (req,res)=>{
-    const formData= req.body;
-    formSchema
-    .validate(formData)
-    .catch(err => {
-        console.log(err.errors);
-    })
-    .then(valid=>{
-        if (valid){
-            console.log("form is good");
-        }
-    });
+    validateForm(req,res)
+});
+router.post("/register", (req,res)=>{
+    validateForm(req,res)
 });
 
 module.exports = router;
