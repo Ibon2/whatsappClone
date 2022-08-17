8644f93c3ab80abed6e5d5231f22cba2384e7979
Ibon2 <ibonlkz@gmail.com>
2022-08-17 20:28:30 +0200

    part 9 donee

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const validateForm = require("../controlers/validateForm");
 const router = express.Router();
-const { handleLogin, attemptLogin, attemptRegister } = require("../controlers/authController");
+const {
+  handleLogin,
+  attemptLogin,
+  attemptRegister,
+} = require("../controlers/authController");
+const { rateLimiter } = require("../controlers/rateLimiter");
 
-router.route("/login").get(handleLogin).post(validateForm, attemptLogin);
-router.post("/register", validateForm, attemptRegister);
+router.route("/login").get(handleLogin).post(validateForm,rateLimiter(60,10), attemptLogin);
+router.post("/register", validateForm, rateLimiter(30,4), attemptRegister);
 
 module.exports = router;
