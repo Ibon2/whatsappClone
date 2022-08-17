b5d5458b866720a439d4865e571576210bb858c8
Ibon2 <ibonlkz@gmail.com>
2022-08-17 19:53:58 +0200

    part 8 finished

--- server/controlers/validateForm.js
+++ server/controlers/validateForm.js
@@ -12,16 +12,20 @@ const formSchema = Yup.object({
         .max(28, 'Password too large'),
 });
 
-const validateForm = (req,res) =>{
+const validateForm = (req,res,next) =>{
     const formData= req.body;
     formSchema
     .validate(formData)
     .catch(err => {
+        res.status(422).send();
         console.log(err.errors);
     })
     .then(valid=>{
         if (valid){
             console.log("form is good");
+            next();
+        }else{
+            res.status(422).send();
         }
     });
 }
